4eadb5e7e9082755fa0c48ca16c074c65b5da64e
ViFeduk <viktor2005fedchenkov@mail.ru>
2024-03-17 20:22:42 +0300

    Добавьте файлы проекта.

---
+++ modules_8.3/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace modules_8._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string catalog = @"C:\newPapka";
+            DeleteAllFileInCatalog(catalog);
+
+        }
+
+        static void DeleteAllFileInCatalog(string path)
+        {
+            if (Directory.Exists(path))
+            {
+                string[] files = Directory.GetFiles(path);
+                string[] subDirectories = Directory.GetDirectories(path);
+
+                DateTime dateTime = DateTime.Now;
+                TimeSpan timeSpan = TimeSpan.FromMinutes(30);
+                
+
+                    foreach (string file in files)
+                    {
+                        FileInfo fileInfo = new FileInfo(file);
+                        TimeSpan accessTime = dateTime - fileInfo.LastAccessTime;
+                        if (accessTime > timeSpan)
+                        {
+                            File.Delete(file);
+                            Console.WriteLine($"Удалён файл {file}");
+                        }
+                    }
+                
+                foreach (string subDirectory in subDirectories)
+                {
+                    DirectoryInfo directoryInfo = new DirectoryInfo(subDirectory);
+                    TimeSpan accessTimeCatalog = dateTime - directoryInfo.LastAccessTime;
+                    if (accessTimeCatalog > timeSpan)
+                    {
+                        directoryInfo.Delete(true);
+                        Console.WriteLine($"Удалён каталог {subDirectory}\n");
+                    }
+                    
+                }
+
+
+            }
+            else Console.WriteLine("Папка не существует");
+        }
+    }
+}
